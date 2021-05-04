99bec73545e7e27e06fab70eaadb620e7f82a2d9
hglinhlinh98 <hlinhlinh140898@gmail.com>
2021-05-04 18:26:36 +0700

    grpc transfer img python server

--- web-toeic/src/main/java/com/webtoeic/service/KetQuaBaiTestService.java
+++
@@ -1,7 +0,0 @@
-package com.webtoeic.service;
-
-import com.webtoeic.entities.KetQuaBaiTest;
-
-public interface KetQuaBaiTestService {
-	void save(KetQuaBaiTest ketquabaitest);
-}
--- web-toeic/src/main/java/com/webtoeic/service/SlideBannerService.java
+++
@@ -1,9 +0,0 @@
-package com.webtoeic.service;
-
-import java.util.List;
-
-import com.webtoeic.entities.SlideBanner;
-
-public interface SlideBannerService {
-	List<SlideBanner> findAll();
-}
